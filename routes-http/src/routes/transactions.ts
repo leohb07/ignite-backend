21ed79c0ef5b893957b93231bf493aa7fae0401a
Leonardo Barrocal <leohb1501@gmail.com>
2024-05-05 21:07:19 -0300

    feat: add middleware to check session id

--- routes-http/src/routes/transactions.ts
+++ routes-http/src/routes/transactions.ts
@@ -3,52 +3,88 @@ import { FastifyInstance } from "fastify";
 import { knex } from "../database/knex";
 import { AmountType } from "../enums/amount-type.enum";
 import { createTransactionValidation } from "../validations/create-transaction.validation";
-import { z } from "zod";
 import { getTransactionParamsSchema } from "../validations/get-transaction.validation";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
 export async function transactionsRoutes(server: FastifyInstance) {
-  server.get("/", async () => {
-    try {
-      const transactions = await knex("transactions").select();
-      return { transactions };
-    } catch (error) {
-      console.log(error);
+  server.get(
+    "/",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      try {
+        const sessionId = request.cookies.sessionId;
+        const transactions = await knex("transactions")
+          .where("session_id", sessionId)
+          .select();
+        return { transactions };
+      } catch (error) {
+        console.log(error);
+      }
     }
-  });
+  );
 
-  server.get("/:id", async (request) => {
-    try {
-      const params = getTransactionParamsSchema.parse(request.params);
-      const transaction = await knex("transactions")
-        .where("id", params.id)
-        .first();
-      return { transaction };
-    } catch (error) {
-      console.error(error);
+  server.get(
+    "/:id",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      try {
+        const sessionId = request.cookies.sessionId;
+        const params = getTransactionParamsSchema.parse(request.params);
+        const transaction = await knex("transactions")
+          .where({
+            id: params.id,
+            session_id: sessionId,
+          })
+          .first();
+        return { transaction };
+      } catch (error) {
+        console.error(error);
+      }
     }
-  });
+  );
 
-  server.get("/summary", async () => {
-    try {
-      const summary = await knex("transactions")
-        .sum("amount", {
-          as: "amount",
-        })
-        .first();
-      return { summary };
-    } catch (error) {
-      console.error(error);
+  server.get(
+    "/summary",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      try {
+        const sessionId = request.cookies.sessionId;
+        const summary = await knex("transactions")
+          .where("session_id", sessionId)
+          .sum("amount", {
+            as: "amount",
+          })
+          .first();
+        return { summary };
+      } catch (error) {
+        console.error(error);
+      }
     }
-  });
+  );
 
   server.post("/", async (request, reply) => {
     const body = createTransactionValidation.parse(request.body);
     try {
+      let sessionId = request.cookies.sessionId;
+      if (!sessionId) {
+        sessionId = randomUUID();
+        reply.cookie("sessionId", sessionId, {
+          path: "/",
+          maxAge: 60 * 60 * 24 * 7, // in seconds7
+        });
+      }
       await knex("transactions").insert({
         id: randomUUID(),
         title: body.title,
         amount:
           body.type === AmountType.credit ? body.amount : body.amount * -1,
+        session_id: sessionId,
       });
       return reply.status(201).send();
     } catch (error) {
